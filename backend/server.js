4b7b4e82b389d0ce13817ea16d286aa69a5c6d6f
Maksims Habibulins <maxhabibulin@gmail.com>
2026-05-03 21:08:23 +0200

    Refactor: move products api endpoint to separate route file

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,14 @@
 import express from "express";
 import cors from "cors";
 import db from "./db.js";
+import productRoutes from "./routes/products.js";
+import ordersRoutes from "./routes/orders.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use("/api/order", ordersRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-app.get("/api/products", (req, res) => {
-  db.all("SELECT * FROM products", [], (err, rows) => {
-    res.json(rows);
-  });
-});
+app.listen(3000, () => console.log("Server started, port: 3000"));
