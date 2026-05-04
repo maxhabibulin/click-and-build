5b3dead0964d2eebf2faa6a6806797b436be3210
Maksims Habibulins <maxhabibulin@gmail.com>
2026-05-04 21:04:44 +0200

    Add: implement order placement with order_items relational mapping

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,4 +3,45 @@ import db from "../db.js";
 
 const router = express.Router();
 
+router.post("/", (req, res) => {
+  const { cart, firstName, lastName, email, address } = req.body;
+
+  let total = 0;
+  cart.forEach((item) => {
+    total += item.product_price * item.quantity;
+  });
+
+  db.run(
+    `
+    INSERT INTO orders (order_total_price, customer_first_name, 
+    customer_last_name, customer_email, customer_address) 
+    VALUES (?, ?, ?, ?, ?)
+    `,
+    [total, firstName, lastName, email, address],
+    function (err) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+
+      const orderID = this.lastID;
+
+      const stmt = db.prepare(`
+        INSERT INTO order_items (order_id, product_id, quantity, unit_price)
+        VALUES (?, ?, ?, ?)
+        `);
+
+      cart.forEach((item) => {
+        stmt.run(orderID, item.id, item.quantity, item.product_price);
+      });
+
+      stmt.finalize();
+
+      res.json({
+        message: "Order created successfully!",
+        orderID: orderID,
+      });
+    },
+  );
+});
+
 export default router;
